2dfba312373cfe5b345814ec20f370d414f799b4
developerpraj <56839366+developerpraj@users.noreply.github.com>
2020-02-26 19:54:57 -0600

    used babel to test ES6 and ES7 converted into ES5 code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,14 @@
 console.log('App.js is running');
 
 
-//JSX -JavaScript XML
+//JSX -JavaScript XML;
 
-var template = <p>This is JSX from app.js</p>;
-
+//var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+    "p", 
+    {id:"someid"} ,
+    "This is JSX from app.js" 
+    );
+var appRoot= document.getElementById('app');
 //rendering the variable in console
-ReactDOM.render(template);
+ReactDOM.render(template,appRoot);
